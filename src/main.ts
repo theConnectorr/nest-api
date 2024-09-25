3923d14a6247a9359c1676b8870ad6ea93f99b1c
theworstkhoi <kylekhoitv@gmail.com>
2024-09-25 14:18:58 +0700

    chore: added hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => console.log("App is listening at http://localhost:3000"));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
